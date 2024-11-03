899a63deea05cef90b911faa2941cb767ed3df31
krishabasrur123 <krishabasrur@gmail.com>
2024-11-02 23:42:03 -0700

    Created a sign-in.js page for front-end

---
+++ src/screens/sign-in.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function App() {
+  const [activeTab, setActiveTab] = useState(null);
+  function openTab(tab) {
+    setActiveTab(tab);
+}
+
+  return (
+    <div>
+      <header className="App-header">
+        <div className="column-left">
+          <p className="p-welcome"> welcome to</p>
+          <h1 className="head-title">Josie's List</h1>
+          <p className="p-byUcla" > made by UCLA students</p>
+          <p className="p-forUcla" > for UCLA students</p>
+          </div>
+
+     
+          <div>
+              {activeTab === 'tab1' && <div className="tab"> Sign In tab</div>}
+              {activeTab === 'tab2' && <div className="tab">Create an Account Tab</div>}
+            </div>
+     
+     
+        <div className="column-right">
+          <p className="p-sign" >sign in</p>
+          <p className="p-google" > with google (@g.ucla.edu)</p>
+          <button className="button sign-in" onClick={() => openTab('tab1')}>Sign In</button>
+          <p className="p-or" >or</p>
+          <button className="button create-account" onClick={() => openTab('tab2')}>Create An Account</button>
+          
+        </div>
+       
+     
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
