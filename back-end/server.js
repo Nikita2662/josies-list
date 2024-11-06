169c90d6fbe498d8b0e8151251b00c75350a62f5
sarah darzacq <sarahdarzacq22@gmail.com>
2024-11-06 13:44:43 -0800

    add controllers folder

--- back-end/server.js
+++ back-end/server.js
@@ -1,44 +1,49 @@
 //Database-Server
 //Adding API Methods by importing MERN npm packages
+require('dotenv').config()
 const express=require("express");
 const mongoose = require('mongoose');
 const cors=require("cors");
 const multer=require("multer");
 const app = express();
-//Create an instance of an Express app
 app.use(cors());  
-
-
-//Adding routes for connection to front-end
-// app.get('/', (req, res) => {
-//   res.send('this is the root node'); 
-// });
-
-const usersRoute = require('./routes/users.js');
-const productsRoute = require('./routes/products.js');
-
-app.use('/users', usersRoute)
-app.use('/products', productsRoute)
-
-// // const port = 3000;
-
-// app.listen(port, () => {
-//   console.log('Node.js HTTP server is running on port ' + port);
-// });
-// //
+const Product = require('./models/Product.js'); 
 
 //Create a connection with the MongoDB
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://Admin:lCNavxtIML74MxCj@josies-list-cluster.o5u3b.mongodb.net/?retryWrites=true&w=majority&appName=josies-list-cluster', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
+  
+//const usersRoute = require('./routes/users.js');
+//const productsRoute = require('./routes/products.js');
+
+//app.use('/users', usersRoute)
+//app.use('/products', productsRoute)
+
+// // const port = 3000;
+
 
 // Define routes
-app.get('/', (req, res) => {
-  res.send('The server works!!');
+// app.get('/api', (req, res) => {
+//   res.json({message: 'Hello from Server!'});
+// });
+
+const port = process.env.PORT || 5038;
+// only listen for requests if successfully connected
+mongoose.connection.once('open', () => {
+  app.listen(port, () => {
+    console.log('Server started on port ' + port);
+    const testItem = new Product();
+    testItem.set('itemName', 'Coffee Table');
+    testItem.set('price', 5); 
+    testItem.save()
+      .then(savedItem => console.log("Item Saved: ", savedItem.itemName))
+      .catch(err => console.log("Error Saving: ", err))
+    console.log(testItem); 
+  })
 });
 
-// Start the server
-const port = process.env.PORT || 5038;
-app.listen(port, 5038)
+
+
