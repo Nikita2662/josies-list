981e3097503012f1c9c35f6656b7778b95ab853b
krishabasrur123 <krishabasrur@gmail.com>
2024-11-09 17:07:15 -0800

    Change App.js to now include the Listing's page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import Landing from "./screens/Landing";
 import CreateAccount from "./screens/CreateAccount";
 import SignIn from './screens/SignIn'; 
+import Listings from "./screens/Listings";
 
 function App() {
-  return <SignIn/>;
+  return <Listings/>;
 }
 
 
