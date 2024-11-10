7648939a54751ce9f67f7298fb14d661673b7523
krishabasrur123 <krishabasrur@gmail.com>
2024-11-09 17:06:02 -0800

    Set up Listings page

---
+++ front-end/src/screens/Listings.js
@@ -0,0 +1,26 @@
+import Header from "../components/Header";
+import SafeArea from "../components/SafeArea";
+import "./Listings.css";
+import React, { useState } from 'react';
+
+function Listings(){
+
+
+    return (
+        <div>
+            <Header/>
+         <SafeArea>
+    <p>Helo World</p>
+    <div class="flex-container">
+    <div style={{ flexGrow: 0 }}>1</div>
+    <div style={{ flexGrow: 1 }}>2</div>
+    <div style={{ flexGrow: 8 }}>3</div>
+    </div>
+    </SafeArea>
+    </div>
+    );
+   
+
+}
+
+export default Listings;
