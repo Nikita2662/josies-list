44580d1c9a7e6a327518366a97da9725ed5f30cb
krishabasrur123 <krishabasrur@gmail.com>
2024-11-09 19:20:53 -0800

    Added a autoscrolling feature to the textboxes so they can expand

--- front-end/src/components/Comments.js
+++ front-end/src/components/Comments.js
@@ -4,13 +4,15 @@ import "./Comments.css"
 
 const Comment= ({ productId }) => {
 
+  
+
     return(
     <div className='flexComment'>
    <div> <h1>Comment</h1></div>
     <div> <TextBox  label="Author Name"
-        placeholder="Type your comment here..."  className="text-box"/></div>
+        placeholder="Type your comment here..."  className="text-box"  /></div>
     <div> <TextBox  label="Author Name"
-        placeholder="Type your comment here..."  className="text-box"/></div>
+        placeholder="Type your comment here..."  className="text-box" /></div>
     
     </div>);
 }
--- front-end/src/components/TextBox.js
+++ front-end/src/components/TextBox.js
@@ -3,22 +3,34 @@ import "./TextBox.css";
 
 const TextBox = ({ label, placeholder, className }) => {
     const [text, setText] = useState('');
+    const [textareaheight, setTextareaheight] = useState(1);
 
     const handleChange = (event) => {
         setText(event.target.value); 
+        
+      const textarea=event.target;
+      textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+
     };
 
+    
+
+
     return (
-        <div className={className}>
-            {label && <label>{label}</label>} {/* Render label if provided */}
-            <input
-                type="text"
-                value={text}
-                onChange={handleChange}
-                placeholder={placeholder}
-                className="text-box"
-            />
-        </div>
+        
+    <div className={className}>
+      {label && <label>{label}</label>} {/* Render label if provided */}
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder={placeholder}
+        className="text-box"
+
+        
+      />
+
+    </div>
     );
 };
 
