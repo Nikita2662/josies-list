5fb9b1db3f205208c47a963978f0ffbec72ce762
krishabasrur123 <krishabasrur@gmail.com>
2024-11-11 23:36:39 -0800

    fixed the image import, initially did not import, now fixed with a arrow function rather than normal function

--- front-end/src/components/Uploadimage.js
+++ front-end/src/components/Uploadimage.js
@@ -6,23 +6,22 @@ function UploadImage() {
     const [image, setImage] = useState(imgUrl);
 
     // Handler for file selection
-    const openImg = (event) => {
-        const file = event.target.files[0];  // Get the selected file
-        if (file) {
-            // Create a temporary URL for the selected file
-            setImage(URL.createObjectURL(file));
-        }
-    };
+    
 
     return (
         <div>
-            {/* Display selected image or placeholder */}
+           
             <img src={image} alt="Uploaded" width={120} height={120} />
-            
-            {/* Input field for file selection */}
+  
             <input 
-                type="file" 
-                onChange={openImg} // Trigger openImg when the user selects a file
+                type="file"
+               
+                onChange={(event) => {
+        if (event.target.files[0]) {
+            // Create a temporary URL for the selected file
+            setImage(URL.createObjectURL(event.target.files[0]));
+        }
+    }} // Trigger openImg when the user selects a file
             />
         </div>
     );
