e7d39b68dff27a51f863c174a1f3fd4c91cf386b
Schuyler Henry <schenry823@gmail.com>
2024-11-12 23:41:19 -0800

    Filters dropdown
    Next: integrate with backend search

--- front-end/src/screens/Search.js
+++ front-end/src/screens/Search.js
@@ -5,17 +5,27 @@ import SearchIcon from "../components/icons/SearchIcon.js";
 
 import "./Search.css";
 
+//TODO - Integrate with backend search API
 function Search() {
   return (
     <>
       <Header />
       <SafeArea>
         <div className="search-bar">
-          <input placeholder="search"></input>
+          <input placeholder="search" />
           <button>
             <SearchIcon size={50} />
           </button>
         </div>
+        <select className="filters-button">
+          <option value="" disabled selected>
+            filters
+          </option>
+          <option value="none">None</option>
+          <option value="clothing">Clothing</option>
+          <option value="dorm">Dorm</option>
+          <option value="tech">Tech</option>
+        </select>
       </SafeArea>
     </>
   );
