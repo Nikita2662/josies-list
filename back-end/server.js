568e353954d158b870bdcc7b4043bee6a51d6293
krishabasrur123 <krishabasrur@gmail.com>
2024-11-18 12:45:38 -0800

    Get the latest changes from dev/back/end

--- back-end/server.js
+++ back-end/server.js
@@ -2,19 +2,20 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose');
-//const Product = require('./models/Product.js'); 
-const Products = require('./routes/productRoutes');
+const Product = require('./models/Product.js'); 
+const Comment = require('./models/Comment.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors()); 
 app.use(express.json());
-app.use('/products', require('./routes/productRoutes'));
+app.use('/', require('./routes/productRoutes'));
+app.use('/', require('./routes/commentRoutes'));
 
 
 // MongoDB connection
-mongoose.connect(process.env.DATABASE_URI)
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
@@ -22,17 +23,16 @@ mongoose.connect(process.env.DATABASE_URI)
 mongoose.connection.once('open', () => {
   app.listen(port, () => {
     console.log('Server started on port ' + port);
-    // const testItem = new Product();
-    // testItem.set('itemName', 'Coffee Table');
-    // testItem.set('price', 5); 
-    // testItem.save()
-    //   .then(savedItem => console.log("Item Saved: ", savedItem.itemName))
-    //   .catch(err => console.log("Error Saving: ", err))
-    // console.log(testItem); 
+    const testItem = new Product();
+    testItem.set('itemName', 'Coffee Table');
+    testItem.set('price', 5); 
+    testItem.set('seller', 'Sarah');
+    testItem.set('description', 'the key description');
+    testItem.save()
+      .then(savedItem => console.log("Item Saved: ", savedItem.itemName))
+      .catch(err => console.log("Error Saving: ", err))
+    console.log(testItem); 
   })
 });
 
 module.exports = app; 
-
-
-
