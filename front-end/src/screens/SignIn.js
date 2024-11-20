2a930040786c0092be279d5828ff87c09580b274
Schuyler Henry <schenry823@gmail.com>
2024-11-19 22:57:59 -0800

    Can get email from google

--- front-end/src/screens/SignIn.js
+++ front-end/src/screens/SignIn.js
@@ -1,56 +1,64 @@
+import React from "react";
+import { useGoogleLogin } from "@react-oauth/google";
+
 import Header from "../components/Header";
 import BlueFlag from "../components/blueFlag.png";
 import GreenFlag from "../components/greenFlag.png";
 import Logo from "../components/Logo";
+import SafeArea from "../components/SafeArea";
 
-
-import './SignIn.css';
-import React, { useState } from 'react';
-
-
-
+import "./SignIn.css";
 
 function SignIn() {
-  const [activeTab, setActiveTab] = useState(null);
-  function openTab(tab) {
-    setActiveTab(tab);
-}
+  const createAccountClick = useGoogleLogin({
+    onSuccess: (tokenResponse) => createAccount(tokenResponse),
+  });
+
+  async function createAccount(tokenResponse) {
+    //get the email
+    let accessToken = tokenResponse.access_token;
+    let response = await fetch(
+      "https://oauth2.googleapis.com/tokeninfo?access_token=" + accessToken
+    );
+    let data = await response.json();
+    let email = data.email;
+    console.log(email);
+
+    //create an account on the backend
+  }
 
   return (
-    <div>
-     <Header /> 
-      <header className="header">
-        <div className="column-left">
-          <p className="p-welcome"> welcome to</p>
-          <h1 className="head-title">Josie's List</h1>
-          <p className="p-byUcla" > made by UCLA students</p>
-          <p className="p-forUcla" > for UCLA students</p>
-          <img  src={GreenFlag} className ="greenFlag"alt="Description of the image" />
-          <Logo size={150} className="p-logo" />
+    <>
+      <Header />
+      <SafeArea>
+        <header className="header">
+          <div className="column-left">
+            <p className="p-welcome"> welcome to</p>
+            <h1 className="head-title">Josie's List</h1>
+            <p className="p-byUcla"> made by UCLA students</p>
+            <p className="p-forUcla"> for UCLA students</p>
+            <img src={GreenFlag} className="greenFlag" alt="" />
+            <Logo size={150} className="p-logo" />
           </div>
-
-     
-          <div>
-              {activeTab === 'tab1' && <div className="tab"> Sign In tab</div>}
-              {activeTab === 'tab2' && <div className="tab">Create an Account Tab</div>}
-            </div>
-     
-     
-        <div className="column-right">
-          <p className="p-sign" >sign in</p>
-          <img src={BlueFlag} alt="Description of the image" />
-          <p className="p-google" > with google (@g.ucla.edu)</p>
-          <button className="button sign-in" onClick={() => openTab('tab1')}>Sign In</button>
-          <p className="button p-or" >-or-</p>
-          <button className="button create-account" onClick={() => openTab('tab2')}>Create An Account</button>
-          
-        </div>
-       
-     
-      </header>
-    </div>
+          <div className="column-right">
+            <p className="p-sign">sign in</p>
+            <img src={BlueFlag} alt="" />
+            <p className="p-google"> with google (@g.ucla.edu)</p>
+            <button className="button sign-in" onClick={() => alert("Sign In")}>
+              Sign In
+            </button>
+            <p className="button p-or">-or-</p>
+            <button
+              className="button create-account"
+              onClick={() => createAccountClick()}
+            >
+              Create An Account
+            </button>
+          </div>
+        </header>
+      </SafeArea>
+    </>
   );
 }
-
 
 export default SignIn;
