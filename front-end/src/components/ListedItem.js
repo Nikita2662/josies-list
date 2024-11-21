c30004e1134a812bf69ddb792a20a4c21ea19944
= <ninarkerr@gmail.com>
2024-11-20 22:12:30 -0800

    let user click on product listing image and route them to specific product page based on id

--- front-end/src/components/ListedItem.js
+++ front-end/src/components/ListedItem.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./ListedItem.css";
 
-function ListedItem({ className = "listed-photo", src, itemName, price }) {
+function ListedItem({ className = "listed-photo", src, itemName, price, _id }) {
+  const navigate = useNavigate();
+
+  const goToProduct = (id) => {
+    navigate(`/product/${id}`);
+  };
+
   return (
     <div className="listed-item">
-      <img className={className} src={src} alt="Listed product" />
-      <h2 className="listing-text">{itemName}</h2>
-      <h2 className="price-text">${price}</h2>
+      <img 
+        className={className} 
+        src={src} 
+        alt="Listed product" 
+        onClick={() => goToProduct(_id)} 
+        style="cursor: pointer;" />
+      <h2 
+        className="listing-text">{itemName}
+      </h2>
+      <h2 
+        className="price-text">${price}
+      </h2>
     </div>
   );
 }
