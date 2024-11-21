f1fbc381a4434db3ff364382a56a4b7114c86eda
Schuyler Henry <schenry823@gmail.com>
2024-11-20 17:30:11 -0800

    User can be created! Not ready for merge to main yet
    Questions: Storing user locally once signed in? Standardizing field names for types?

--- front-end/src/screens/CreateAccount.js
+++ front-end/src/screens/CreateAccount.js
@@ -5,42 +5,77 @@ import "./CreateAccount.css";
 import Button from "../components/Button.js";
 import TextBox from "../components/TextBox.js";
 import { useLocation } from "react-router-dom";
+import { UserContext } from "../App.js";
 
 function CreateAccount() {
   // add app.use(express.json()); in backend? for JSON input
 
   const [bio, setBio] = useState("");
+  const [name, setName] = useState("");
+
   const location = useLocation();
   const email = location.state.email;
+
+  const { setUser } = React.useContext(UserContext);
 
   const createUser = async (event) => {
     event.preventDefault();
 
-    try {
-      const updatedUser = {
-        username: email,
-        bio: bio,
-      };
+    const body1 = {
+      _id: email,
+    };
 
-      const response = await fetch(`http://localhost:5000/users/${email}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Send data as JSON
-        },
-        body: JSON.stringify(updatedUser), // Convert object to JSON string
-      });
+    //REMOVE THIS LATER
+    const delResponse = await fetch("http://localhost:5000/users/del", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body1),
+    });
+    console.log(await delResponse.json());
+    //STOP REMOVING HERE
 
-      if (!response.ok) {
-        throw new Error("Failed to update user");
-      }
+    const response1 = await fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body1),
+    });
 
-      const data = await response.json();
-      console.log("User updated successfully:", data);
-      alert(`User updated: ${email}`);
-    } catch (error) {
-      console.error("Error sending data:", error);
-      alert("Failed to update user.");
+    if (response1.status === 400) {
+      throw new Error(
+        "User already exists in database, I don't know how we got here"
+      );
     }
+
+    const body2 = {
+      bio: bio,
+      name: name,
+    };
+
+    const response2 = await fetch(`http://localhost:5000/users/${email}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body2),
+    });
+
+    if (response2.status === 400) {
+      throw new Error(
+        "Somehow the user was not created but we got here anyways"
+      );
+    }
+
+    let user = {
+      _id: email,
+      bio: bio,
+      name: name,
+    };
+
+    setUser(user);
   };
 
   const handleClick = () => {
@@ -68,6 +103,8 @@ function CreateAccount() {
               label="Name"
               placeholder="Josie Bruin"
               className="text-box-container-top"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
             <TextBox
               label="Email"
