2478a621d972d1191d1aefdc5e3e94fd4147de07
Nikita2662 <nikita.senthil@gmail.com>
2024-12-02 20:43:54 -0800

    changed server port back to 5038

--- back-end/server.js
+++ back-end/server.js
@@ -11,7 +11,7 @@ const commentRoutes = require("./routes/commentRoutes");
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5038;
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
